f43a7aca7f2b9c2300f608989fbddb376e0623c6
Jose Martin Rangel Espinoza <jose.rangelespinoza@mercadolibre.com.mx>
2021-07-28 18:07:54 -0500

    python profesional

---
+++ decorador.py
@@ -0,0 +1,14 @@
+def run():
+    print(mensaje('Cesar'))
+
+def mayusculas(func):
+    def envoltura(texto):
+        return func(texto).upper()
+    return envoltura
+
+@mayusculas
+def mensaje(nombre):
+    return f'{nombre}, recibiste un mensaje'
+
+if __name__ == '__main__':
+    run()
